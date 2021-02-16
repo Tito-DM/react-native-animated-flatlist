c1992b1983e6adbfa91f4d25d7c637cd3113f063
Pyong <muandatito@gmail.com>
2021-02-16 02:12:04 +0000

    create the first flatlist

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Easing,
+  SafeAreaViewBase,
+  SafeAreaView,
+} from "react-native";
+const { width, height } = Dimensions.get("screen");
 
-export default function App() {
+const API_KEY = "563492ad6f9170000100000104d7c3f8d7004020a13eff916c6eb7a9";
+const API_URL =
+  "https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20";
+const fetchImg = async () => {
+  const data = await fetch(API_URL, {
+    headers: {
+      Authorization: API_KEY,
+    },
+  });
+
+  const {photos} = await data.json();
+  return photos;
+};
+export default () => {
+  const [img, setimg] = React.useState(null)
+  React.useEffect(()=>{
+        const fetchData = async ()=>{
+          const img = await fetchImg();
+          setimg(img)
+        }
+
+        fetchData();
+  },[])
+
+  if(!img){
+    return null
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <FlatList 
+      horizontal
+      pagingEnabled
+      showsHorizontalScrollIndicator={false}
+      data= {img} 
+      keyExtractor={item=> item.id.toString()} 
+      renderItem={({item})=>{
+        return (
+    
+            <View style= {{width,height}}>
+              <Image source ={{uri: item.src.portrait}} style={[StyleSheet.absoluteFillObject]} />
+            </View>
+        )
+      }}/>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
